ee273934e3ac56750257ead2cd5c846edc39547a
Bartosz Taudul <wolf.pld@gmail.com>
2017-09-20 22:35:10 +0200

    Const variables.

--- server/TracyView.cpp
+++ server/TracyView.cpp
@@ -600,11 +600,11 @@ void View::DrawFrames()
         idx += group;
     }
 
-    auto zitbegin = std::lower_bound( m_frames.begin(), m_frames.end(), m_zvStart );
-    auto zitend = std::upper_bound( m_frames.begin(), m_frames.end(), m_zvEnd );
+    const auto zitbegin = std::lower_bound( m_frames.begin(), m_frames.end(), m_zvStart );
+    const auto zitend = std::upper_bound( m_frames.begin(), m_frames.end(), m_zvEnd );
 
-    auto zbegin = (int)std::distance( m_frames.begin(), zitbegin );
-    auto zend = (int)std::distance( m_frames.begin(), zitend );
+    const auto zbegin = (int)std::distance( m_frames.begin(), zitbegin );
+    const auto zend = (int)std::distance( m_frames.begin(), zitend );
 
     if( zend > m_frameStart && zbegin < m_frameStart + onScreen * group )
     {

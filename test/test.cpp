b89adbaf8cff7e6bc160d35c80eca4cebd1b1629
Bartosz Taudul <wolf.pld@gmail.com>
2017-09-23 21:37:14 +0200

    Add second scope test.

--- test/test.cpp
+++ test/test.cpp
@@ -29,16 +29,26 @@ void ResolutionCheck()
 
 }
 
+void ScopeCheck()
+{
+    for(;;)
+    {
+        std::this_thread::sleep_for( std::chrono::milliseconds( 1 ) );
+        ZoneScoped;
+    }
+}
 
 int main()
 {
     auto t1 = std::thread( TestFunction );
     auto t2 = std::thread( TestFunction );
     auto t3 = std::thread( ResolutionCheck );
+    auto t4 = std::thread( ScopeCheck );
 
     tracy::SetThreadName( t1, "First thread" );
     tracy::SetThreadName( t2, "Second thread" );
     tracy::SetThreadName( t3, "Resolution check" );
+    tracy::SetThreadName( t4, "Scope check" );
 
     for(;;)
     {

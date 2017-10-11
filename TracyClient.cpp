14cd87c42d7808238048519940a1beb123d58669
Bartosz Taudul <wolf.pld@gmail.com>
2017-10-11 02:09:52 +0200

    Add (optional) fast-integration source file.

---
+++ TracyClient.cpp
@@ -0,0 +1,17 @@
+//
+//          Tracy profiler
+//         ----------------
+//
+// For fast integration, compile and
+// link with this source file (and none
+// other).
+//
+
+#include "client/TracyProfiler.cpp"
+#include "common/tracy_lz4.cpp"
+#include "common/TracySocket.cpp"
+#include "common/TracySystem.cpp"
+
+#ifdef _MSC_VER
+#  pragma comment(lib, "ws2_32.lib")
+#endif

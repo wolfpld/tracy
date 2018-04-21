ea2be1bce98310ba7f8b7a70b4b5a5acb243779e
Bartosz Taudul <wolf.pld@gmail.com>
2018-04-21 23:19:13 +0200

    Fix custom ImVec2 operators.

--- server/TracyImGui.hpp
+++ server/TracyImGui.hpp
@@ -4,7 +4,7 @@
 #include "../imgui/imgui.h"
 #include "../imgui/imgui_internal.h"
 
-ImVec2 operator+( const ImVec2& l, const ImVec2& r ) { return ImVec2( l.x + r.x, l.y + r.y ); }
-ImVec2 operator-( const ImVec2& l, const ImVec2& r ) { return ImVec2( l.x - r.x, l.y - r.y ); }
+static inline ImVec2 operator+( const ImVec2& l, const ImVec2& r ) { return ImVec2( l.x + r.x, l.y + r.y ); }
+static inline ImVec2 operator-( const ImVec2& l, const ImVec2& r ) { return ImVec2( l.x - r.x, l.y - r.y ); }
 
 #endif

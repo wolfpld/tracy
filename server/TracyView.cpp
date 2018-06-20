cef972fe2578fca28ce8071ccd0463ba8a3731f6
Bartosz Taudul <bartosz.taudul@game-lion.com>
2018-06-20 17:07:48 +0200

    Remove parenthesis from callstack location.

--- server/TracyView.cpp
+++ server/TracyView.cpp
@@ -4804,7 +4804,7 @@ void View::DrawCallstackWindow()
             ImGui::TextWrapped( "%s", m_worker.GetString( frame->name ) );
             ImGui::NextColumn();
             ImGui::PushTextWrapPos( 0.0f );
-            ImGui::TextDisabled( "(%s:%i)", m_worker.GetString( frame->file ), frame->line );
+            ImGui::TextDisabled( "%s:%i", m_worker.GetString( frame->file ), frame->line );
             ImGui::PopTextWrapPos();
             ImGui::NextColumn();
         }

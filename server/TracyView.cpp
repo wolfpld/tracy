96042891f702eac7519906b097acb71175147190
Bartosz Taudul <bartosz.taudul@game-lion.com>
2018-07-13 12:30:29 +0200

    Reintroduce explicit template type for std::lock_guard.
    Requested in issue #4 for support of older MSVC versions.

--- server/TracyView.cpp
+++ server/TracyView.cpp
@@ -360,7 +360,7 @@ bool View::DrawImpl()
         keepOpenPtr = &keepOpen;
     }
 
-    std::lock_guard lock( m_worker.GetDataLock() );
+    std::lock_guard<NonRecursiveBenaphore> lock( m_worker.GetDataLock() );
     char tmp[2048];
     sprintf( tmp, "%s###Profiler", m_worker.GetCaptureName().c_str() );
     ImGui::SetNextWindowSize( ImVec2( 1550, 800 ), ImGuiCond_FirstUseEver );
@@ -433,7 +433,7 @@ void View::DrawConnection()
     const auto cs = ty * 0.9f;
 
     {
-        std::lock_guard lock( m_worker.GetMbpsDataLock() );
+        std::lock_guard<NonRecursiveBenaphore> lock( m_worker.GetMbpsDataLock() );
         ImGui::Begin( m_worker.GetAddr().c_str(), nullptr, ImGuiWindowFlags_AlwaysAutoResize );
         const auto& mbpsVector = m_worker.GetMbpsData();
         const auto mbps = mbpsVector.back();
@@ -457,7 +457,7 @@ void View::DrawConnection()
     const auto wpos = ImGui::GetWindowPos() + ImGui::GetWindowContentRegionMin();
     ImGui::GetWindowDrawList()->AddCircleFilled( wpos + ImVec2( 1 + cs * 0.5, 3 + ty * 0.5 ), cs * 0.5, m_worker.IsConnected() ? 0xFF2222CC : 0xFF444444, 10 );
 
-    std::lock_guard lock( m_worker.GetDataLock() );
+    std::lock_guard<NonRecursiveBenaphore> lock( m_worker.GetDataLock() );
     {
         const auto sz = m_worker.GetFrameCount();
         if( sz > 1 )

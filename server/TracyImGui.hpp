064385fc628e01ce33676f782e54a40178d48568
Rokas Kupstys <rokups@zoho.com>
2018-07-17 15:58:31 +0300

    Define ImVec2 math operators only when IMGUI_DEFINE_MATH_OPERATORS is undefined.

--- server/TracyImGui.hpp
+++ server/TracyImGui.hpp
@@ -4,7 +4,9 @@
 #include "../imgui/imgui.h"
 #include "../imgui/imgui_internal.h"
 
+#if !IMGUI_DEFINE_MATH_OPERATORS
 static inline ImVec2 operator+( const ImVec2& l, const ImVec2& r ) { return ImVec2( l.x + r.x, l.y + r.y ); }
 static inline ImVec2 operator-( const ImVec2& l, const ImVec2& r ) { return ImVec2( l.x - r.x, l.y - r.y ); }
+#endif
 
 #endif

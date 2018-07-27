68e40ad2504333e3e42d4f07bab9fd74398ff4d7
Bartosz Taudul <wolf.pld@gmail.com>
2018-07-28 01:40:48 +0200

    Unix build can use extended font.

--- standalone/build/unix/build.mk
+++ standalone/build/unix/build.mk
@@ -1,6 +1,6 @@
 CFLAGS += 
 CXXFLAGS := $(CFLAGS) -std=c++17
-DEFINES += -DTRACY_FILESELECTOR
+DEFINES += -DTRACY_FILESELECTOR -DTRACY_EXTENDED_FONT
 INCLUDES := $(shell pkg-config --cflags glfw3) -I../../../imgui -I../../libs/gl3w
 LIBS := $(shell pkg-config --libs glfw3) -lpthread -ldl
 PROJECT := Tracy

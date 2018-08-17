e1e0e6e1403368a166db7644fe3ca0de166fd6d6
Bartosz Taudul <wolf.pld@gmail.com>
2018-08-17 23:07:58 +0200

    Centered text helper.

--- server/TracyImGui.hpp
+++ server/TracyImGui.hpp
@@ -9,4 +9,16 @@ static inline ImVec2 operator+( const ImVec2& l, const ImVec2& r ) { return ImVe
 static inline ImVec2 operator-( const ImVec2& l, const ImVec2& r ) { return ImVec2( l.x - r.x, l.y - r.y ); }
 #endif
 
+namespace tracy
+{
+
+    static inline void TextCentered( const char* text )
+    {
+        const auto tw = ImGui::CalcTextSize( text ).x;
+        ImGui::SetCursorPosX( ( ImGui::GetWindowWidth() - tw ) * 0.5f );
+        ImGui::Text( text );
+    }
+
+}
+
 #endif

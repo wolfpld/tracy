e1821e439ab324390bc70e9424cd8480cef6dd22
Bartosz Taudul <wolf.pld@gmail.com>
2018-08-19 02:59:12 +0200

    Add icons to error pop-ups.

--- server/TracyBadVersion.cpp
+++ server/TracyBadVersion.cpp
@@ -1,5 +1,6 @@
 #include <assert.h>
 
+#include "IconsFontAwesome5.h"
 #include "TracyBadVersion.hpp"
 #include "TracyImGui.hpp"
 
@@ -23,6 +24,9 @@ void BadVersionImpl( int& badVer )
     }
     if( ImGui::BeginPopupModal( "Unsupported file version", nullptr, ImGuiWindowFlags_AlwaysAutoResize ) )
     {
+#ifdef TRACY_EXTENDED_FONT
+        TextCentered( ICON_FA_CLOUD_DOWNLOAD_ALT );
+#endif
         ImGui::Text( "The file you are trying to open is unsupported.\nYou should update to tracy %i.%i.%i or newer and try again.", badVer >> 16, ( badVer >> 8 ) & 0xFF, badVer & 0xFF );
         ImGui::Separator();
         if( ImGui::Button( "I understand" ) )
@@ -34,6 +38,9 @@ void BadVersionImpl( int& badVer )
     }
     if( ImGui::BeginPopupModal( "Bad file", nullptr, ImGuiWindowFlags_AlwaysAutoResize ) )
     {
+#ifdef TRACY_EXTENDED_FONT
+        TextCentered( ICON_FA_EXCLAMATION_TRIANGLE );
+#endif
         ImGui::Text( "The file you are trying to open is not a tracy dump." );
         ImGui::Separator();
         if( ImGui::Button( "Oops" ) )

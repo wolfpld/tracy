0e6350d95e1d8b05df15ce62710afc6a34f17742
Bartosz Taudul <wolf.pld@gmail.com>
2019-02-06 23:09:38 +0100

    Grouping by function names is a more sane default.

--- server/TracyView.hpp
+++ server/TracyView.hpp
@@ -295,8 +295,8 @@ private:
     float m_notificationTime = 0;
     std::string m_notificationText;
 
-    bool m_groupCallstackTreeByNameBottomUp = false;
-    bool m_groupCallstackTreeByNameTopDown = false;
+    bool m_groupCallstackTreeByNameBottomUp = true;
+    bool m_groupCallstackTreeByNameTopDown = true;
 
     struct FindZone {
         enum : uint64_t { Unselected = std::numeric_limits<uint64_t>::max() - 1 };

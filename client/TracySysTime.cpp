963d2b3ca8823fecf4a45dbb51ab91c85a9964ff
Bartosz Taudul <bartosz.taudul@game-lion.com>
2019-02-25 15:04:06 +0100

    CPU usage getter for apple.

--- client/TracySysTime.cpp
+++ client/TracySysTime.cpp
@@ -8,6 +8,9 @@
 #    include <assert.h>
 #    include <stdio.h>
 #    include <inttypes.h>
+#  elif defined __APPLE__
+#    include <mach/mach_host.h>
+#    include <mach/host_info.h>
 #  endif
 
 namespace tracy
@@ -46,6 +49,17 @@ void SysTime::ReadTimes()
     used = user + nice + system;
 }
 
+#  elif defined __APPLE__
+
+void SysTime::ReadTimes()
+{
+    host_cpu_load_info_data_t info;
+    mach_msg_type_number_t cnt = HOST_CPU_LOAD_INFO_COUNT;
+    host_statistics( mach_host_self(), HOST_CPU_LOAD_INFO, reinterpret_cast<host_info_t>( &info ), &cnt ); 
+    used = info.cpu_ticks[CPU_STATE_USER] + info.cpu_ticks[CPU_STATE_NICE] + info.cpu_ticks[CPU_STATE_SYSTEM];
+    idle = info.cpu_ticks[CPU_STATE_IDLE];
+}
+
 #endif
 
 SysTime::SysTime()
@@ -65,7 +79,7 @@ float SysTime::Get()
 
 #if defined _WIN32 || defined __CYGWIN__
     return diffUsed == 0 ? 0 : ( diffUsed - diffIdle ) * 100.f / diffUsed;
-#elif defined __linux__
+#elif defined __linux__ || defined __APPLE__
     const auto total = diffUsed + diffIdle;
     return total == 0 ? 0 : diffUsed * 100.f / total;
 #endif

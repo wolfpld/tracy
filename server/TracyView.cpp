9563c8316d66d50a8b7835db7a76bc1cd703c4e4
Bartosz Taudul <wolf.pld@gmail.com>
2019-03-09 14:20:34 +0100

    Optimize lock drawing.
    Don't iterate over locks that are present in only one thread, if only
    contended lock events are to be displayed. Such locks cannot be
    contended.

--- server/TracyView.cpp
+++ server/TracyView.cpp
@@ -3128,6 +3128,7 @@ int View::DrawLocks( uint64_t tid, bool hover, double pxns, const ImVec2& wpos, 
     {
         const auto& lockmap = v.second;
         if( !lockmap.valid || !Vis( &lockmap ).visible ) continue;
+        if( m_onlyContendedLocks && lockmap.threadList.size() == 1 && m_lockInfoWindow != v.first ) continue;
 
         auto it = lockmap.threadMap.find( tid );
         if( it == lockmap.threadMap.end() ) continue;

1a32edebf2402e0111cd261d4036a75cc3fdaec9
Bartosz Taudul <wolf.pld@gmail.com>
2019-06-18 20:43:28 +0200

    Extract text printing functions.

--- capture/src/capture.cpp
+++ capture/src/capture.cpp
@@ -13,6 +13,7 @@
 #include "../../common/TracyProtocol.hpp"
 #include "../../server/TracyFileWrite.hpp"
 #include "../../server/TracyMemory.hpp"
+#include "../../server/TracyPrint.hpp"
 #include "../../server/TracyWorker.hpp"
 #include "getopt.h"
 
@@ -25,87 +26,6 @@ void SigInt( int )
     disconnect = true;
 }
 #endif
-
-static const char* TimeToString( int64_t ns )
-{
-    enum { Pool = 8 };
-    static char bufpool[Pool][64];
-    static int bufsel = 0;
-    char* buf = bufpool[bufsel];
-    bufsel = ( bufsel + 1 ) % Pool;
-
-    const char* sign = "";
-    if( ns < 0 )
-    {
-        sign = "-";
-        ns = -ns;
-    }
-
-    if( ns < 1000 )
-    {
-        sprintf( buf, "%s%" PRIi64 " ns", sign, ns );
-    }
-    else if( ns < 1000ll * 1000 )
-    {
-        sprintf( buf, "%s%.2f us", sign, ns / 1000. );
-    }
-    else if( ns < 1000ll * 1000 * 1000 )
-    {
-        sprintf( buf, "%s%.2f ms", sign, ns / ( 1000. * 1000. ) );
-    }
-    else if( ns < 1000ll * 1000 * 1000 * 60 )
-    {
-        sprintf( buf, "%s%.2f s", sign, ns / ( 1000. * 1000. * 1000. ) );
-    }
-    else
-    {
-        const auto m = int64_t( ns / ( 1000ll * 1000 * 1000 * 60 ) );
-        const auto s = int64_t( ns - m * ( 1000ll * 1000 * 1000 * 60 ) );
-        sprintf( buf, "%s%" PRIi64 ":%04.1f", sign, m, s / ( 1000. * 1000. * 1000. ) );
-    }
-    return buf;
-}
-
-static const char* RealToString( double val, bool separator )
-{
-    enum { Pool = 8 };
-    static char bufpool[Pool][64];
-    static int bufsel = 0;
-    char* buf = bufpool[bufsel];
-    bufsel = ( bufsel + 1 ) % Pool;
-
-    sprintf( buf, "%f", val );
-    auto ptr = buf;
-    if( *ptr == '-' ) ptr++;
-
-    const auto vbegin = ptr;
-
-    if( separator )
-    {
-        while( *ptr != '\0' && *ptr != ',' && *ptr != '.' ) ptr++;
-        auto end = ptr;
-        while( *end != '\0' ) end++;
-        auto sz = end - ptr;
-
-        while( ptr - vbegin > 3 )
-        {
-            ptr -= 3;
-            memmove( ptr+1, ptr, sz );
-            *ptr = ',';
-            sz += 4;
-        }
-    }
-
-    while( *ptr != '\0' && *ptr != ',' && *ptr != '.' ) ptr++;
-
-    if( *ptr == '\0' ) return buf;
-    while( *ptr != '\0' ) ptr++;
-    ptr--;
-    while( *ptr == '0' && *ptr != ',' && *ptr != '.' ) ptr--;
-    if( *ptr != '.' && *ptr != ',' ) ptr++;
-    *ptr = '\0';
-    return buf;
-}
 
 
 void Usage()
@@ -169,7 +89,7 @@ int main( int argc, char** argv )
         }
     }
     while( !worker.HasData() ) std::this_thread::sleep_for( std::chrono::milliseconds( 100 ) );
-    printf( "\nQueue delay: %s\nTimer resolution: %s\n", TimeToString( worker.GetDelay() ), TimeToString( worker.GetResolution() ) );
+    printf( "\nQueue delay: %s\nTimer resolution: %s\n", tracy::TimeToString( worker.GetDelay() ), tracy::TimeToString( worker.GetResolution() ) );
 
 #ifndef _MSC_VER
     struct sigaction sigint;
@@ -203,7 +123,7 @@ int main( int argc, char** argv )
         {
             printf( "\33[2K\r\033[36;1m%7.2f Mbps", mbps );
         }
-        printf( " \033[0m /\033[36;1m%5.1f%% \033[0m=\033[33;1m%7.2f Mbps \033[0m| Mem: \033[31;1m%.2f MB\033[0m | \033[33mTime: %s\033[0m", compRatio * 100.f, mbps / compRatio, tracy::memUsage.load( std::memory_order_relaxed ) / ( 1024.f * 1024.f ), TimeToString( worker.GetLastTime() - worker.GetTimeBegin() ) );
+        printf( " \033[0m /\033[36;1m%5.1f%% \033[0m=\033[33;1m%7.2f Mbps \033[0m| Mem: \033[31;1m%.2f MB\033[0m | \033[33mTime: %s\033[0m", compRatio * 100.f, mbps / compRatio, tracy::memUsage.load( std::memory_order_relaxed ) / ( 1024.f * 1024.f ), tracy::TimeToString( worker.GetLastTime() - worker.GetTimeBegin() ) );
         fflush( stdout );
 
         std::this_thread::sleep_for( std::chrono::milliseconds( 100 ) );
@@ -215,7 +135,7 @@ int main( int argc, char** argv )
         printf( "\n\033[31;1mInstrumentation failure: %s\033[0m", tracy::Worker::GetFailureString( failure ) );
     }
 
-    printf( "\nFrames: %" PRIu64 "\nTime span: %s\nZones: %s\nSaving trace...", worker.GetFrameCount( *worker.GetFramesBase() ), TimeToString( worker.GetLastTime() - worker.GetTimeBegin() ), RealToString( worker.GetZoneCount(), true ) );
+    printf( "\nFrames: %" PRIu64 "\nTime span: %s\nZones: %s\nSaving trace...", worker.GetFrameCount( *worker.GetFramesBase() ), tracy::TimeToString( worker.GetLastTime() - worker.GetTimeBegin() ), tracy::RealToString( worker.GetZoneCount(), true ) );
     fflush( stdout );
     auto f = std::unique_ptr<tracy::FileWrite>( tracy::FileWrite::Open( output ) );
     if( f )

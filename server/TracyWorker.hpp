a194c937400418f37eae1ee5224acd6d93731f12
Bartosz Taudul <wolf.pld@gmail.com>
2019-08-14 20:26:55 +0200

    Allow checking if context switch data is available.

--- server/TracyWorker.hpp
+++ server/TracyWorker.hpp
@@ -278,6 +278,7 @@ public:
     uint64_t GetLockCount() const;
     uint64_t GetPlotCount() const;
     uint64_t GetContextSwitchCount() const;
+    bool HasContextSwitches() const { return !m_data.ctxSwitch.empty(); }
     uint64_t GetSrcLocCount() const { return m_data.sourceLocationPayload.size() + m_data.sourceLocation.size(); }
     uint64_t GetCallstackPayloadCount() const { return m_data.callstackPayload.size() - 1; }
     uint64_t GetCallstackFrameCount() const { return m_data.callstackFrameMap.size(); }

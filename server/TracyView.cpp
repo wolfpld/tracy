c5a6c7bf638f73e11a98e92cabde3afc3e3ef067
Bartosz Taudul <wolf.pld@gmail.com>
2019-10-24 00:47:25 +0200

    Display transferred data size.

--- server/TracyView.cpp
+++ server/TracyView.cpp
@@ -946,6 +946,7 @@ bool View::DrawConnection()
         ImGui::SameLine();
         ImGui::PlotLines( buf, mbpsVector.data(), mbpsVector.size(), 0, nullptr, 0, std::numeric_limits<float>::max(), ImVec2( 150, 0 ) );
         ImGui::Text( "Ratio %.1f%%  Real: %6.2f Mbps", m_worker.GetCompRatio() * 100.f, mbps / m_worker.GetCompRatio() );
+        ImGui::Text( "Data transferred: %s", MemSizeToString( m_worker.GetDataTransferred() ) );
         ImGui::Text( "Query backlog: %s", RealToString( m_worker.GetSendQueueSize(), true ) );
     }
 

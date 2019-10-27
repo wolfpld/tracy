106411e1f606e422a67261175474b745429a8b96
Bartosz Taudul <wolf.pld@gmail.com>
2019-10-27 13:39:01 +0100

    Add missing freeaddrinfo().

--- common/TracySocket.cpp
+++ common/TracySocket.cpp
@@ -310,8 +310,9 @@ bool ListenSocket::Listen( int port, int backlog )
     int val = 1;
     setsockopt( m_sock, SOL_SOCKET, SO_REUSEADDR, &val, sizeof( val ) );
 #endif
-    if( bind( m_sock, res->ai_addr, res->ai_addrlen ) == -1 ) return false;
-    if( listen( m_sock, backlog ) == -1 ) return false;
+    if( bind( m_sock, res->ai_addr, res->ai_addrlen ) == -1 ) { freeaddrinfo( res ); return false; }
+    if( listen( m_sock, backlog ) == -1 ) { freeaddrinfo( res ); return false; }
+    freeaddrinfo( res );
     return true;
 }
 

3050f19e0b2ff2ce2f500bc5ffe773e70c8c08f6
Bartosz Taudul <wolf.pld@gmail.com>
2020-01-31 01:30:33 +0100

    Leverage std::to_chars.

--- server/TracyPrint.hpp
+++ server/TracyPrint.hpp
@@ -1,7 +1,11 @@
 #ifndef __TRACYPRINT_HPP__
 #define __TRACYPRINT_HPP__
 
-#include <stdio.h>
+#if ( defined _MSC_VER && _MSVC_LANG >= 201703L ) || __cplusplus >= 201703L
+#  include <charconv>
+#else
+#  include <stdio.h>
+#endif
 
 namespace tracy
 {
@@ -9,7 +13,11 @@ namespace tracy
 template<typename T>
 static inline char* PrintFloat( char* begin, char* end, T value, int precision )
 {
+#if ( defined _MSC_VER && _MSVC_LANG >= 201703L ) || __cplusplus >= 201703L
+    return std::to_chars( begin, end, value, std::chars_format::fixed, precision ).ptr;
+#else
     return begin + sprintf( begin, "%.*f", precision, value );
+#endif
 }
 
 const char* TimeToString( int64_t ns );

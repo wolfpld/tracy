0d2e7f72a740b6a62f509295ceb472035e32737a
Bartosz Taudul <bartosz.taudul@game-lion.com>
2020-02-11 14:40:49 +0100

    Move GetThreadHandleImpl() from header to source file.
    This removes dependency on unistd.h header (required for syscall() on
    linux), which includes a definition of getopt(), which may conflict with
    a custom getopt implementation (for example, one that does work on
    windows).

--- common/TracySystem.cpp
+++ common/TracySystem.cpp
@@ -18,10 +18,16 @@
 #endif
 
 #ifdef __linux__
-#  ifndef __ANDROID__
-#    include <syscall.h>
+#  ifdef __ANDROID__
+#    include <sys/types.h>
+#  else
+#    include <sys/syscall.h>
 #  endif
 #  include <fcntl.h>
+#elif defined __FreeBSD__
+#  include <sys/thr.h>
+#elif defined __NetBSD__ || defined __DragonFly__
+#  include <sys/lwp.h>
 #endif
 
 #ifdef __MINGW32__
@@ -40,6 +46,41 @@
 
 namespace tracy
 {
+
+namespace detail
+{
+
+TRACY_API uint64_t GetThreadHandleImpl()
+{
+#if defined _WIN32 || defined __CYGWIN__
+    static_assert( sizeof( decltype( GetCurrentThreadId() ) ) <= sizeof( uint64_t ), "Thread handle too big to fit in protocol" );
+    return uint64_t( GetCurrentThreadId() );
+#elif defined __APPLE__
+    uint64_t id;
+    pthread_threadid_np( pthread_self(), &id );
+    return id;
+#elif defined __ANDROID__
+    return (uint64_t)gettid();
+#elif defined __linux__
+    return (uint64_t)syscall( SYS_gettid );
+#elif defined __FreeBSD__
+    long id;
+    thr_self( &id );
+    return id;
+#elif defined __NetBSD__
+    return _lwp_self();
+#elif defined __DragonFly__
+    return lwp_gettid();
+#elif defined __OpenBSD__
+    return getthrid();
+#else
+    static_assert( sizeof( decltype( pthread_self() ) ) <= sizeof( uint64_t ), "Thread handle too big to fit in protocol" );
+    return uint64_t( pthread_self() );
+#endif
+
+}
+
+}
 
 #ifdef TRACY_ENABLE
 struct ThreadNameData

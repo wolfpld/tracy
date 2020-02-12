cc805b7b7437e5cefcbb6e071633f5a47b8963a3
Bartosz Taudul <wolf.pld@gmail.com>
2020-02-12 19:22:46 +0100

    Add mmap() wrapper.

---
+++ server/TracyMmap.cpp
@@ -0,0 +1,38 @@
+#include "TracyMmap.hpp"
+
+#if defined _MSC_VER || defined __MINGW32__ || defined __CYGWIN__
+#  include <io.h>
+#  include <windows.h>
+
+void* mmap( void* addr, size_t length, int prot, int flags, int fd, off_t offset )
+{
+    HANDLE hnd;
+    void* map = nullptr;
+
+    switch( prot )
+    {
+    case PROT_READ:
+        if( hnd = CreateFileMapping( HANDLE( _get_osfhandle( fd ) ), nullptr, PAGE_READONLY, 0, 0, nullptr ) )
+        {
+            map = MapViewOfFile( hnd, FILE_MAP_READ, 0, 0, length );
+            CloseHandle( hnd );
+        }
+        break;
+    case PROT_WRITE:
+        if( hnd = CreateFileMapping( HANDLE( _get_osfhandle( fd ) ), nullptr, PAGE_READWRITE, 0, 0, nullptr ) )
+        {
+            map = MapViewOfFile( hnd, FILE_MAP_WRITE, 0, 0, length );
+            CloseHandle( hnd );
+        }
+        break;
+    }
+
+    return map ? (char*)map + offset : (void*)-1;
+}
+
+int munmap( void* addr, size_t length )
+{
+    return UnmapViewOfFile( addr ) != 0 ? 0 : -1;
+}
+
+#endif

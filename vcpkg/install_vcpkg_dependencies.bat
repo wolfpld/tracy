979f41efd1696f560f167f27092fad1cc2ee630c
ikrima <contact@ikrima.com>
2020-05-02 14:39:58 -0700

    Add -disableMetrics to vcpkg script

--- vcpkg/install_vcpkg_dependencies.bat
+++ vcpkg/install_vcpkg_dependencies.bat
@@ -4,7 +4,7 @@ REM get vcpkg distribution
 if not exist vcpkg git clone https://github.com/Microsoft/vcpkg.git
 
 REM build vcpkg
-if not exist vcpkg\vcpkg.exe call vcpkg\bootstrap-vcpkg.bat
+if not exist vcpkg\vcpkg.exe call vcpkg\bootstrap-vcpkg.bat -disableMetrics
 
 REM install required packages
 vcpkg\vcpkg.exe install --triplet x64-windows-static freetype glfw3 capstone[arm,arm64,x86]

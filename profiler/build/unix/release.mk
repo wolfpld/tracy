884de148c988e8ecd1e34e1a2548e12201c249a4
Bartosz Taudul <wolf.pld@gmail.com>
2020-05-13 18:35:56 +0200

    Target native architecture.

--- profiler/build/unix/release.mk
+++ profiler/build/unix/release.mk
@@ -1,11 +1,7 @@
 ARCH := $(shell uname -m)
 
-CFLAGS := -O3 -s
+CFLAGS := -O3 -s -march=native
 DEFINES := -DNDEBUG
 BUILD := release
-
-ifeq ($(ARCH),x86_64)
-CFLAGS += -msse4.1
-endif
 
 include build.mk

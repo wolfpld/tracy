wolfpld/tracy

commit a110b420111ce02825fffaec29d08b5cc7b2168a

Add ___tracy_init_thread function
This function needs to be called for each non-main thread before calling
the `___tracy_alloc_` functions.

Alternative way to achieve this could be initializing the allocator
transparently in the `___tracy_alloc_*` calls.

client/TracyProfiler.cpp
@@ -3068,6 +3068,13 @@ TRACY_API void ___tracy_emit_message_appinfo( const char* txt, size_t size ) { t
 TRACY_API uint64_t ___tracy_alloc_srcloc( uint32_t line, const char* source, const char* function ) { return tracy::Profiler::AllocSourceLocation( line, source, function ); }
 TRACY_API uint64_t ___tracy_alloc_srcloc_name( uint32_t line, const char* source, const char* function, const char* name, size_t nameSz ) { return tracy::Profiler::AllocSourceLocation( line, source, function, name, nameSz ); }
 
+// thread_locals are not initialized on thread creation. At least on GNU/Linux. Instead they are
+// initialized on their first ODR-use. This means that the allocator is not automagically
+// initialized every time a thread is created. As thus, expose to the C API users a simple API to
+// call every time they create a thread. Here we can then put all sorts of per-thread
+// initialization.
+TRACY_API void ___tracy_init_thread(void) { (void)tracy::s_rpmalloc_thread_init; }
+
 #ifdef __cplusplus
 }
 #endif

TracyC.h
@@ -81,6 +81,7 @@ struct ___tracy_c_zone_context
 // This struct, as visible to user, is immutable, so treat it as if const was declared here.
 typedef /*const*/ struct ___tracy_c_zone_context TracyCZoneCtx;
 
+TRACY_API void ___tracy_init_thread(void);
 TRACY_API uint64_t ___tracy_alloc_srcloc( uint32_t line, const char* source, const char* function );
 TRACY_API uint64_t ___tracy_alloc_srcloc_name( uint32_t line, const char* source, const char* function, const char* name, size_t nameSz );
 

manual/tracy.tex
@@ -1477,6 +1477,9 @@ couleur=black!5,
 logo=\bcbombe
 ]{Important}
 Since you are directly calling the profiler functions here, you will need to take care of manually disabling the code, if the \texttt{TRACY\_ENABLE} macro is not defined.
+
+Additionally, make sure you have called \texttt{\_\_\_tracy\_init\_thread} before calling the
+\textttt{alloc} functions for the first time on a new non-main thread.
 \end{bclogo}
 
 \subsubsection{Memory profiling}

commit b03da81afa99913db177675c6936c8f8134b962f

Fix build on macos
It was broken by a110b420111ce02825fffaec29d08b5cc7b2168a.

In addition, adjust the CI definition to test builds on macOS.

.github/workflows/gcc.yml
@@ -9,22 +9,30 @@ on:
 jobs:
   build:
 
-    runs-on: ubuntu-latest
+    runs-on: ${{ matrix.os }}
+    strategy:
+      fail-fast: false
+      matrix:
+        os: [ubuntu-latest, macOS-latest]
 
     steps:
     - uses: actions/checkout@v2
-    - name: Install libraries
+    - name: Install linux libraries
+      if: ${{ matrix.os == 'ubuntu-latest' }}
       run: sudo apt-get update && sudo apt-get -y install libglfw3-dev libgtk2.0-dev libcapstone-dev
+    - name: Install macos libraries
+      if: ${{ matrix.os == 'macOS-latest' }}
+      run: brew install capstone tbb pkg-config glfw
     - name: Profiler GUI
-      run: make -C profiler/build/unix debug release
+      run: make -j -C profiler/build/unix debug release
     - name: Update utility
-      run: make -C update/build/unix debug release
+      run: make -j -C update/build/unix debug release
     - name: Capture utility
-      run: make -C capture/build/unix debug release
+      run: make -j -C capture/build/unix debug release
     - name: Library
-      run: make -C library/unix debug release
+      run: make -j -C library/unix debug release
     - name: Test application
       run: |
-        make -C test
-        make -C test clean
-        make -C test TRACYFLAGS=-DTRACY_ON_DEMAND
+        make -j -C test
+        make -j -C test clean
+        make -j -C test TRACYFLAGS=-DTRACY_ON_DEMAND

client/TracyProfiler.cpp
@@ -3073,7 +3073,13 @@ TRACY_API uint64_t ___tracy_alloc_srcloc_name( uint32_t line, const char* source
 // initialized every time a thread is created. As thus, expose to the C API users a simple API to
 // call every time they create a thread. Here we can then put all sorts of per-thread
 // initialization.
-TRACY_API void ___tracy_init_thread(void) { (void)tracy::s_rpmalloc_thread_init; }
+TRACY_API void ___tracy_init_thread(void) {
+#ifdef TRACY_DELAYED_INIT
+    (void)tracy::GetProfilerThreadData();
+#else
+    (void)tracy::s_rpmalloc_thread_init;
+#endif
+}
 
 #ifdef __cplusplus
 }

update/src/update.cpp
@@ -16,6 +16,8 @@
 
 #ifdef __CYGWIN__
 #  define ftello64(x) ftello(x)
+#elif defined __APPLE__
+#  define ftello64(x) ftello(x)
 #elif defined _WIN32
 #  define ftello64(x) _ftelli64(x)
 #endif
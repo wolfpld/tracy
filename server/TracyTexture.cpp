3e3f8aaabff63e81b8bbbd79b8db9b11ca2c2708
Bartosz Taudul <wolf.pld@gmail.com>
2020-07-29 13:31:26 +0200

    Enable linear filtering for frame images.

--- server/TracyTexture.cpp
+++ server/TracyTexture.cpp
@@ -14,8 +14,8 @@ void* MakeTexture()
     GLuint tex;
     glGenTextures( 1, &tex );
     glBindTexture( GL_TEXTURE_2D, tex );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
     return (void*)(intptr_t)tex;
 }
 

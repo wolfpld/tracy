e406ce5aaba9a0df03380dc933614387e4e3966d
Bartosz Taudul <wolf.pld@gmail.com>
2020-07-29 19:00:37 +0200

    Fix typo.

--- server/TracyView.cpp
+++ server/TracyView.cpp
@@ -1838,7 +1838,7 @@ void View::HandleZoneViewMouse( int64_t timespan, const ImVec2& wpos, float w, d
     }
 
     const auto hwheel_delta = io.MouseWheelH * 100.f;
-    if( ImGui::IsMouseDragging( 1, 0 ) ||  hwheel_delta != 0 )
+    if( ImGui::IsMouseDragging( 1, 0 ) || hwheel_delta != 0 )
     {
         m_pause = true;
         m_zoomAnim.active = false;

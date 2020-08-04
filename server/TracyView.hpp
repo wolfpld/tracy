2acc1d9670e6f0325a463a94f879df2439ff3bb1
Bartosz Taudul <wolf.pld@gmail.com>
2020-08-04 17:09:17 +0200

    Move Range, RangeSlim out of View.

--- server/TracyView.hpp
+++ server/TracyView.hpp
@@ -56,29 +56,6 @@ class View
         uint64_t count;
     };
 
-    struct Range
-    {
-        void StartFrame() { hiMin = hiMax = false; }
-
-        int64_t min = 0;
-        int64_t max = 0;
-        bool active = false;
-        bool hiMin = false;
-        bool hiMax = false;
-        bool modMin = false;
-        bool modMax = false;
-    };
-
-    struct RangeSlim
-    {
-        bool operator==( const Range& other ) const { return other.active == active && other.min == min && other.max == max; }
-        bool operator!=( const Range& other ) const { return !(*this == other); }
-        void operator=( const Range& other ) { active = other.active; min = other.min; max = other.max; }
-
-        int64_t min, max;
-        bool active = false;
-    };
-
 public:
     struct VisData
     {
--- server/TracyViewData.hpp
+++ server/TracyViewData.hpp
@@ -7,6 +7,30 @@
 namespace tracy
 {
 
+struct Range
+{
+    void StartFrame() { hiMin = hiMax = false; }
+
+    int64_t min = 0;
+    int64_t max = 0;
+    bool active = false;
+    bool hiMin = false;
+    bool hiMax = false;
+    bool modMin = false;
+    bool modMax = false;
+};
+
+struct RangeSlim
+{
+    bool operator==( const Range& other ) const { return other.active == active && other.min == min && other.max == max; }
+    bool operator!=( const Range& other ) const { return !(*this == other); }
+    void operator=( const Range& other ) { active = other.active; min = other.min; max = other.max; }
+
+    int64_t min, max;
+    bool active = false;
+};
+
+
 struct ViewData
 {
     int64_t zvStart = 0;

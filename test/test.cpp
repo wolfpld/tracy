35f939165dbb0f04f801a64ca2c7a2575b2ce20f
Bartosz Taudul <wolf.pld@gmail.com>
2020-08-12 23:55:49 +0200

    Don't sleep in test application's fibonacci thread.

--- test/test.cpp
+++ test/test.cpp
@@ -176,7 +176,6 @@ void DepthTest()
     tracy::SetThreadName( "Depth test" );
     for(;;)
     {
-        std::this_thread::sleep_for( std::chrono::milliseconds( 20 ) );
         ZoneScoped;
         const auto txt = "Fibonacci (15)";
         ZoneText( txt, strlen( txt ) );

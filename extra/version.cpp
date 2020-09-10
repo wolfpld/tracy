81dc6c216eeaf71f48a3702bec5462157dc3d254
Bartosz Taudul <wolf.pld@gmail.com>
2020-09-10 20:38:22 +0200

    Add version printer.

---
+++ extra/version.cpp
@@ -0,0 +1,11 @@
+#include <stdint.h>
+#include <stdio.h>
+
+#include "../server/TracyFileHeader.hpp"
+#include "../server/TracyVersion.hpp"
+
+int main()
+{
+    const auto ver = uint32_t( tracy::FileVersion( tracy::Version::Major, tracy::Version::Minor, tracy::Version::Patch ) );
+    fwrite( &ver, 1, 4, stdout );
+}

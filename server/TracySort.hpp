d71d76414b8cc6a3d5658ceed57e540687d1c23e
Bartosz Taudul <wolf@nereid.pl>
2021-04-15 19:55:50 +0200

    Allow setting NO_PARALLEL_SORT externally.

--- server/TracySort.hpp
+++ server/TracySort.hpp
@@ -1,15 +1,17 @@
 #ifndef __TRACYSORT_HPP__
 #define __TRACYSORT_HPP__
 
-#if !defined __APPLE__ && ( ( defined _MSC_VER && _MSVC_LANG >= 201703L ) || __cplusplus >= 201703L )
-#  if __has_include(<execution>)
-#    include <algorithm>
-#    include <execution>
+#ifndef NO_PARALLEL_SORT
+#  if !defined __APPLE__ && ( ( defined _MSC_VER && _MSVC_LANG >= 201703L ) || __cplusplus >= 201703L )
+#    if __has_include(<execution>)
+#      include <algorithm>
+#      include <execution>
+#    else
+#      define NO_PARALLEL_SORT
+#    endif
 #  else
 #    define NO_PARALLEL_SORT
 #  endif
-#else
-#  define NO_PARALLEL_SORT
 #endif
 
 #include "tracy_pdqsort.h"

5a7e9cdbdd1c503d690ad7ef36c19636ed245b65
Bartosz Taudul <wolf@nereid.pl>
2021-05-20 02:31:00 +0200

    0% miss rates are fine too.

--- server/TracySourceView.cpp
+++ server/TracySourceView.cpp
@@ -2636,21 +2636,21 @@ void SourceView::RenderAsmLine( AsmLine& line, const AddrStat& ipcnt, const Addr
                     }
                     if( hw->cycles ) TextFocused( "Cycles:", RealToString( hw->cycles ) );
                     if( hw->retired ) TextFocused( "Retirements:", RealToString( hw->retired ) );
-                    if( hw->cacheRef && hw->cacheMiss )
+                    if( hw->cacheRef )
                     {
                         TextDisabledUnformatted( "Cache miss rate:" );
                         ImGui::SameLine();
                         PrintPercentage( float( 100 * hw->cacheMiss ) / hw->cacheRef );
+                        TextFocused( "Cache references:", RealToString( hw->cacheRef ) );
                     }
-                    if( hw->cacheRef ) TextFocused( "Cache references:", RealToString( hw->cacheRef ) );
                     if( hw->cacheMiss ) TextFocused( "Cache misses:", RealToString( hw->cacheMiss ) );
-                    if( hw->branchRetired && hw->branchMiss )
+                    if( hw->branchRetired )
                     {
                         TextDisabledUnformatted( "Branch mispredictions rate:" );
                         ImGui::SameLine();
                         PrintPercentage( float( 100 * hw->branchMiss ) / hw->branchRetired );
+                        TextFocused( "Retired branches:", RealToString( hw->branchRetired ) );
                     }
-                    if( hw->branchRetired ) TextFocused( "Retired branches:", RealToString( hw->branchRetired ) );
                     if( hw->branchMiss ) TextFocused( "Branch mispredictions:", RealToString( hw->branchMiss ) );
                 }
                 const auto& stats = *worker.GetSymbolStats( symAddrParents );

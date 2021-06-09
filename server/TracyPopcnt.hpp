90b51568e9b6de8d337834d74212241fcd760652
Bartosz Taudul <wolf@nereid.pl>
2021-06-09 20:29:43 +0200

    Add common lzcnt implementation.
    Not really tested, so fingers crossed.

--- server/TracyPopcnt.hpp
+++ server/TracyPopcnt.hpp
@@ -2,14 +2,20 @@
 #define __TRACYPOPCNT_HPP__
 
 #include <limits.h>
+#include <stdint.h>
 
 #if defined _WIN64
 #  include <intrin.h>
 #  define TracyCountBits __popcnt64
+#  define TracyLzcnt __lzcnt64
 #elif defined __GNUC__ || defined __clang__
 static inline uint64_t TracyCountBits( uint64_t i )
 {
     return uint64_t( __builtin_popcountll( i ) );
+}
+static inline uint64_t TracyLzcnt( uint64_t i )
+{
+    return uint64_t( __builtin_clzll( i ) );
 }
 #else
 static inline uint64_t TracyCountBits( uint64_t i )
@@ -18,6 +24,16 @@ static inline uint64_t TracyCountBits( uint64_t i )
     i = ( i & 0x3333333333333333 ) + ( (i >> 2) & 0x3333333333333333 );
     i = ( (i + (i >> 4) ) & 0x0F0F0F0F0F0F0F0F );
     return ( i * (0x0101010101010101) ) >> 56;
+}
+static inline uint64_t TracyLzcnt( uint64_t i )
+{
+    i |= i >> 1;
+    i |= i >> 2;
+    i |= i >> 4;
+    i |= i >> 8;
+    i |= i >> 16;
+    i |= i >> 32;
+    return 64 - TracyCountBits( i );
 }
 #endif
 

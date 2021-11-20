bcdbd2f7d7a90a1331c59f62ab8f366ac702567b
Bartosz Taudul <wolf@nereid.pl>
2021-11-20 15:17:15 +0100

    Add simple fiber example.

---
+++ examples/fibers.cpp
@@ -0,0 +1,30 @@
+// g++ fibers.cpp ../TracyClient.cpp -DTRACY_ENABLE -DTRACY_FIBERS -lpthread -ldl
+
+#include <thread>
+#include <unistd.h>
+
+#include "../Tracy.hpp"
+#include "../TracyC.h"
+
+const char* fiber = "job1";
+TracyCZoneCtx zone;
+
+int main()
+{
+    std::thread t1( [] {
+        TracyFiberEnter( fiber );
+        TracyCZone( ctx, 1 );
+        zone = ctx;
+        sleep( 1 );
+        TracyFiberLeave;
+    });
+    t1.join();
+
+    std::thread t2( [] {
+        TracyFiberEnter( fiber );
+        sleep( 1 );
+        TracyCZoneEnd( zone );
+        TracyFiberLeave;
+    });
+    t2.join();
+}

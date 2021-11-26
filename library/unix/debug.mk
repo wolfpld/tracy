e8627997970aa415f76b21b27252180aa0aa7133
Graydon Hoare <graydon@pobox.com>
2021-11-25 20:33:46 -0800

    Support TRACY_NO_ISA_EXTENSIONS build flag.

--- library/unix/debug.mk
+++ library/unix/debug.mk
@@ -4,8 +4,10 @@ CFLAGS := -g3 -Wall
 DEFINES := -DDEBUG
 BUILD := debug
 
+ifndef TRACY_NO_ISA_EXTENSIONS
 ifeq ($(ARCH),x86_64)
 CFLAGS += -msse4.1
+endif
 endif
 
 include build.mk
--- library/unix/release.mk
+++ library/unix/release.mk
@@ -4,8 +4,10 @@ CFLAGS := -O3 -s
 DEFINES := -DNDEBUG
 BUILD := release
 
+ifndef TRACY_NO_ISA_EXTENSIONS
 ifeq ($(ARCH),x86_64)
 CFLAGS += -msse4.1
+endif
 endif
 
 include build.mk

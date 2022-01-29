361782f3fdbeaefb9697f8fabe9d14e6b5d18f75
Bartosz Taudul <wolf@nereid.pl>
2022-01-29 15:53:50 +0100

    Fix edge case triggered by mouse being out-of-window.

--- server/TracyPrint.cpp
+++ server/TracyPrint.cpp
@@ -257,9 +257,16 @@ const char* TimeToStringExact( int64_t _ns )
         const auto h = int64_t( ns / ( 1000ll * 1000 * 1000 * 60 * 60 ) - d * 24 );
         const auto m = int64_t( ns / ( 1000ll * 1000 * 1000 * 60 ) - d * ( 60 * 24 ) - h * 60 );
         const auto s = int64_t( ns / ( 1000ll * 1000 * 1000 ) - d * ( 60 * 60 * 24 ) - h * ( 60 * 60 ) - m * 60 );
-        assert( d < 100 );
-        PrintTinyInt( buf, d );
-        *buf++ = 'd';
+        if( d < 100 )
+        {
+            PrintTinyInt( buf, d );
+            *buf++ = 'd';
+        }
+        else
+        {
+            memcpy( buf, "100+d", 5 );
+            buf += 5;
+        }
         PrintTinyInt0( buf, h );
         *buf++ = ':';
         PrintTinyInt0( buf, m );

afd4cdec63cbb7e6448613cb319e5d846bca7c66
Bartosz Taudul <wolf@nereid.pl>
2022-04-27 02:33:22 +0200

    LTO + lld wants the compiler to be clang.

--- common/unix-release.mk
+++ common/unix-release.mk
@@ -3,11 +3,11 @@ ARCH := $(shell uname -m)
 ifeq (0,$(shell $(CC) --version | grep clang && echo 1 || echo 0))
 CFLAGS += -s
 else
+  ifeq (1,$(shell ld.lld --version > /dev/null && echo 1 || echo 0))
+LDFLAGS := -s -fuse-ld=lld
+  else
 LDFLAGS := -s
-endif
-
-ifeq (1,$(shell ld.lld --version > /dev/null && echo 1 || echo 0))
-LDFLAGS += -fuse-ld=lld
+  endif
 endif
 
 ifneq (,$(filter $(ARCH),aarch64 arm64))

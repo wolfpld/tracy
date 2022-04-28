31b3212f7656947ce7f2b748ee6fcf2c27593081
Bartosz Taudul <wolf@nereid.pl>
2022-04-28 22:51:56 +0200

    The mold linker is not recognized by gcc.

--- common/unix-debug.mk
+++ common/unix-debug.mk
@@ -1,7 +1,9 @@
 ARCH := $(shell uname -m)
 
-ifeq (1,$(shell ld.mold --version > /dev/null && echo 1 || echo 0))
-LDFLAGS += -fuse-ld=mold
+ifeq (1,$(shell $(CC) --version | grep clang > /dev/null && echo 1 || echo 0))
+  ifeq (1,$(shell ld.mold --version > /dev/null && echo 1 || echo 0))
+LDFLAGS := -fuse-ld=mold
+  endif
 endif
 
 ifeq ($(ARCH),x86_64)

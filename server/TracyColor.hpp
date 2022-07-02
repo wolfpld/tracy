5b8c8c53091f474d991d2376db5e59f4215f5cf5
Bartosz Taudul <wolf@nereid.pl>
2022-07-02 13:10:31 +0200

    Extract common zone algorithms from View.

--- server/TracyColor.hpp
+++ server/TracyColor.hpp
@@ -1,12 +1,24 @@
 #ifndef __TRACYCOLOR_HPP__
 #define __TRACYCOLOR_HPP__
 
+#include <algorithm>
 #include <stdint.h>
+
+#include "../common/TracyForceInline.hpp"
 
 namespace tracy
 {
 
 uint32_t GetHsvColor( uint64_t hue, int value );
+
+template<int V = 25>
+static tracy_force_inline uint32_t HighlightColor( uint32_t color )
+{
+    return 0xFF000000 |
+        ( std::min<int>( 0xFF, ( ( ( color & 0x00FF0000 ) >> 16 ) + V ) ) << 16 ) |
+        ( std::min<int>( 0xFF, ( ( ( color & 0x0000FF00 ) >> 8  ) + V ) ) << 8  ) |
+        ( std::min<int>( 0xFF, ( ( ( color & 0x000000FF )       ) + V ) )       );
+}
 
 }
 

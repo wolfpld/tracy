048e20b68ce70c89107a6d39d58519c627db5657
Bartosz Taudul <wolf@nereid.pl>
2022-09-11 13:09:59 +0200

    Check for macro existence before checking its value.

--- public/common/TracyYield.hpp
+++ public/common/TracyYield.hpp
@@ -1,7 +1,7 @@
 #ifndef __TRACYYIELD_HPP__
 #define __TRACYYIELD_HPP__
 
-#if defined __SSE2__ || defined _M_AMD64 || _M_IX86_FP == 2
+#if defined __SSE2__ || defined _M_AMD64 || (defined _M_IX86_FP && _M_IX86_FP == 2)
 #  include <emmintrin.h>
 #else
 #  include <thread>
@@ -14,7 +14,7 @@ namespace tracy
 
 static tracy_force_inline void YieldThread()
 {
-#if defined __SSE2__ || defined _M_AMD64 || _M_IX86_FP == 2
+#if defined __SSE2__ || defined _M_AMD64 || (defined _M_IX86_FP && _M_IX86_FP == 2)
     _mm_pause();
 #elif defined __aarch64__
     asm volatile( "isb" : : );

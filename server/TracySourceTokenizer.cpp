8da6105ee4a96f36eb95600c4276cfaddd4d6f03
Bartosz Taudul <wolf@nereid.pl>
2022-09-17 23:31:34 +0200

    Cosmetics.

--- server/TracySourceTokenizer.cpp
+++ server/TracySourceTokenizer.cpp
@@ -113,7 +113,7 @@ static unordered_flat_set<const char*, charutil::Hasher, charutil::Comparator> G
 static unordered_flat_set<const char*, charutil::Hasher, charutil::Comparator> GetAsmSizeDirectives()
 {
     unordered_flat_set<const char*, charutil::Hasher, charutil::Comparator> ret;
-    for( auto& v : { "byte", "word", "dword", "qword", "xmmword", "ymmword" })
+    for( auto& v : { "byte", "word", "dword", "qword", "xmmword", "ymmword" } )
     {
         ret.insert( v );
     }

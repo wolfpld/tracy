ba34596b91baaf176d0885e78d991c7675ac5cbb
Bartosz Taudul <wolf@nereid.pl>
2022-10-14 21:36:09 +0200

    No cost propagation when symbol is narrowed down to a single function.

--- server/TracySourceView.cpp
+++ server/TracySourceView.cpp
@@ -1792,7 +1792,7 @@ static uint32_t GetGoodnessColor( float inRatio )
 void SourceView::RenderSymbolSourceView( const AddrStatData& as, Worker& worker, const View& view )
 {
     const auto scale = GetScale();
-    if( m_cost == CostType::SampleCount && ( as.ipTotalAsm.local + as.ipTotalAsm.ext ) > 0 || ( view.m_statRange.active && worker.GetSamplesForSymbol( m_baseAddr ) ) )
+    if( m_cost == CostType::SampleCount && !m_calcInlineStats && ( as.ipTotalAsm.local + as.ipTotalAsm.ext ) > 0 || ( view.m_statRange.active && worker.GetSamplesForSymbol( m_baseAddr ) ) )
     {
         const auto samplesReady = worker.AreSymbolSamplesReady();
         if( !samplesReady )

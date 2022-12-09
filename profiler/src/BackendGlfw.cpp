ee3d5a94a71a898de1c09b9f07c71f1ddd2bfa82
Bartosz Taudul <wolf@nereid.pl>
2022-12-09 18:59:01 +0100

    Fix apfel check.

--- profiler/src/BackendGlfw.cpp
+++ profiler/src/BackendGlfw.cpp
@@ -72,7 +72,7 @@ Backend::Backend( const char* title, std::function<void()> redraw, RunQueue* mai
     glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
     glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 2 );
     glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
-#if __APPLE__
+#ifdef __APPLE__
     glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE );
 #endif
     s_window = glfwCreateWindow( m_winPos.w, m_winPos.h, title, NULL, NULL );

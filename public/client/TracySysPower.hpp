c3e7157cd597aece51e46f001446d46365617822
Bartosz Taudul <wolf@nereid.pl>
2023-03-10 00:05:18 +0100

    Detect power domains.

--- public/client/TracySysPower.hpp
+++ public/client/TracySysPower.hpp
@@ -7,16 +7,34 @@
 
 #ifdef TRACY_HAS_SYSPOWER
 
+#include <stdint.h>
+#include <stdio.h>
+
+#include "TracyFastVector.hpp"
+
 namespace tracy
 {
 
 class SysPower
 {
+    struct Domain
+    {
+        uint64_t value;
+        uint64_t overflow;
+        FILE* handle;
+        const char* name;
+    };
+
 public:
     SysPower();
     ~SysPower();
 
     void Tick();
+
+private:
+    void ScanDirectory( const char* path, int parent );
+
+    FastVector<Domain> m_domains;
 };
 
 }

efce727ad49131ff350fd5e8fe98108497368460
Bartosz Taudul <wolf@nereid.pl>
2023-05-01 15:13:00 +0200

    Add extern "C" to ini.h.

--- profiler/src/ini.h
+++ profiler/src/ini.h
@@ -10,11 +10,19 @@
 
 #define INI_VERSION "0.1.1"
 
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
 typedef struct ini_t ini_t;
 
 ini_t*      ini_load(const char *filename);
 void        ini_free(ini_t *ini);
 const char* ini_get(ini_t *ini, const char *section, const char *key);
 int         ini_sget(ini_t *ini, const char *section, const char *key, const char *scanfmt, void *dst);
+
+#if defined(__cplusplus)
+}
+#endif
 
 #endif

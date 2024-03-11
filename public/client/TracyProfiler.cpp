a26f076a0efd626ebbf614bdba3ba738b9bd2f50
Elliot <35050275+apache-hb@users.noreply.github.com>
2024-03-11 14:44:44 -0400

    fix build warnings when compiling with clang-cl

--- public/client/TracyProfiler.cpp
+++ public/client/TracyProfiler.cpp
@@ -510,7 +510,7 @@ static const char* GetHostInfo()
 #  ifdef __MINGW32__
         ptr += sprintf( ptr, "OS: Windows %i.%i.%i (MingW)\n", (int)ver.dwMajorVersion, (int)ver.dwMinorVersion, (int)ver.dwBuildNumber );
 #  else
-        ptr += sprintf( ptr, "OS: Windows %i.%i.%i\n", ver.dwMajorVersion, ver.dwMinorVersion, ver.dwBuildNumber );
+        ptr += sprintf( ptr, "OS: Windows %lu.%lu.%lu\n", ver.dwMajorVersion, ver.dwMinorVersion, ver.dwBuildNumber );
 #  endif
     }
 #elif defined __linux__

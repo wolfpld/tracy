375f4526dfc2d3c93680689b752da47b0eaeccc6
Bartosz Taudul <wolf@nereid.pl>
2024-03-17 13:19:06 +0100

    Set TRACY_VERSION_STRING directly in version.cmake.

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -11,12 +11,14 @@ string(REGEX MATCH "Major = ([0-9]+)" _ ${version})
 # This works do to the above () subexpression selection. See
 # https://cmake.org/cmake/help/latest/command/string.html#regex-match for more
 # details
-set(major ${CMAKE_MATCH_1})
+set(TRACY_VERSION_MAJOR ${CMAKE_MATCH_1})
 
 string(REGEX MATCH "Minor = ([0-9]+)" _ ${version})
-set(minor ${CMAKE_MATCH_1})
+set(TRACY_VERSION_MINOR ${CMAKE_MATCH_1})
 
 string(REGEX MATCH "Patch = ([0-9]+)" _ ${version})
-set(patch ${CMAKE_MATCH_1})
+set(TRACY_VERSION_PATCH ${CMAKE_MATCH_1})
 
-message("VERSION ${major}.${minor}.${patch}")
+set(TRACY_VERSION_STRING "${TRACY_VERSION_MAJOR}.${TRACY_VERSION_MINOR}.${TRACY_VERSION_PATCH}")
+
+message("VERSION ${TRACY_VERSION_STRING}")

7f8a2e2c9a8581c2263da77222f1194ee0653e48
Bartosz Taudul <wolf@nereid.pl>
2024-03-19 22:28:02 +0100

    Remove old build files.

--- capture/build/unix/release.mk
+++
@@ -1,9 +0,0 @@
-CFLAGS := -O3
-ifndef TRACY_NO_LTO
-CFLAGS += -flto
-endif
-DEFINES := -DNDEBUG
-BUILD := release
-
-include ../../../common/unix-release.mk
-include build.mk
--- import-chrome/build/unix/debug.mk
+++
@@ -1,6 +0,0 @@
-CFLAGS := -g3 -Wall
-DEFINES := -DDEBUG
-BUILD := debug
-
-include ../../../common/unix-debug.mk
-include build.mk

b5fda4f456089990738c72daba8d0df7ac934356
Bartosz Taudul <wolf@nereid.pl>
2024-06-07 22:47:36 +0200

    Add achievement data structure definitions.

--- profiler/src/profiler/TracyAchievements.hpp
+++ profiler/src/profiler/TracyAchievements.hpp
@@ -1,11 +1,45 @@
 #ifndef __TRACYACHIEVEMENTS_HPP__
 #define __TRACYACHIEVEMENTS_HPP__
 
+#include <stdint.h>
 #include <string>
 #include <vector>
 
 namespace tracy
 {
+
+namespace data
+{
+
+struct ctx
+{
+    ImFont* big;
+    ImFont* small;
+    ImFont* fixed;
+};
+
+struct AchievementItem
+{
+    const char* id;
+    const char* name;
+    void(*description)(const ctx&);
+    AchievementItem** items;
+    bool keepOpen;
+    uint64_t unlockTime;
+    uint64_t doneTime;
+    bool hideCompleted;
+    bool hideNew;
+};
+
+struct AchievementCategory
+{
+    const char* id;
+    const char* name;
+    AchievementItem** items;
+    uint64_t unlockTime;
+};
+
+}
 
 class AchievementsMgr
 {

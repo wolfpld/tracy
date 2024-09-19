f971faab5898bffe08b954eb671944e714a71cf1
Bartosz Taudul <wolf@nereid.pl>
2024-09-19 20:53:44 +0200

    Fix horizontal wheel scroll direction.

--- profiler/src/BackendEmscripten.cpp
+++ profiler/src/BackendEmscripten.cpp
@@ -200,7 +200,7 @@ Backend::Backend( const char* title, const std::function<void()>& redraw, const 
         return EM_TRUE;
     } );
     emscripten_set_wheel_callback( "#canvas", nullptr, EM_TRUE, []( int, const EmscriptenWheelEvent* e, void* ) -> EM_BOOL {
-        ImGui::GetIO().AddMouseWheelEvent( e->deltaX, -e->deltaY );
+        ImGui::GetIO().AddMouseWheelEvent( -e->deltaX, -e->deltaY );
         tracy::s_wasActive = true;
         return EM_TRUE;
     } );

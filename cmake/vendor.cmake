3ac95e6902e79a5082894fca398ad19893e452f7
Bartosz Taudul <wolf@nereid.pl>
2025-02-03 19:54:39 +0100

    Bump imgui to 1.91.8.

--- cmake/vendor.cmake
+++ cmake/vendor.cmake
@@ -169,7 +169,7 @@ target_include_directories(TracyGetOpt PUBLIC ${GETOPT_DIR})
 CPMAddPackage(
     NAME ImGui
     GITHUB_REPOSITORY ocornut/imgui
-    GIT_TAG v1.91.6-docking
+    GIT_TAG v1.91.8-docking
     DOWNLOAD_ONLY TRUE
     PATCHES
         "${CMAKE_CURRENT_LIST_DIR}/imgui-emscripten.patch"

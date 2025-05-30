87924acdd99f97a58e08ef7cd0c17a7f8543caa1
Bartosz Taudul <wolf@nereid.pl>
2025-05-30 20:27:13 +0200

    Release 0.12.0.

--- profiler/src/profiler/TracyProtoHistory.cpp
+++ profiler/src/profiler/TracyProtoHistory.cpp
@@ -5,6 +5,7 @@ namespace tracy
 {
 
 constexpr ProtocolHistory_t ProtocolHistoryArr[] = {
+    { 74, FileVersion( 0, 12, 0 ) },
     { 69, FileVersion( 0, 11, 1 ) },
     { 66, FileVersion( 0, 11, 0 ) },
     { 64, FileVersion( 0, 10, 0 ) },

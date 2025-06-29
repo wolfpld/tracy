6c05079e7b04341fe31595d6a2c15063a62bd5e1
Bartosz Taudul <wolf@nereid.pl>
2025-06-29 14:53:37 +0200

    Disable LLM settings when the model is responding.

--- profiler/src/profiler/TracyLlm.cpp
+++ profiler/src/profiler/TracyLlm.cpp
@@ -161,6 +161,8 @@ void TracyLlm::Draw()
     ImGui::SameLine();
     if( ImGui::TreeNode( "Settings" ) )
     {
+        const auto responding = m_responding;
+        if( responding ) ImGui::BeginDisabled();
         ImGui::Spacing();
         ImGui::AlignTextToFramePadding();
         TextDisabledUnformatted( "API:" );
@@ -273,6 +275,7 @@ void TracyLlm::Draw()
         ImGui::SameLine();
         ImGui::SetNextItemWidth( 40 * scale );
         if( ImGui::InputFloat( "##temperature", &m_temperature, 0, 0, "%.2f" ) ) m_temperature = std::clamp( m_temperature, 0.f, 2.f );
+        if( responding ) ImGui::EndDisabled();
 
         ImGui::Checkbox( ICON_FA_GLOBE " Internet access", &m_tools->m_netAccess );
 

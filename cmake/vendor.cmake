9079baf0b3cf7f6b3d9d1c4848fe4239843899df
Bartosz Taudul <wolf@nereid.pl>
2026-02-17 22:01:39 +0100

    Bump capstone to 6.0.0-Alpha7.

--- cmake/vendor.cmake
+++ cmake/vendor.cmake
@@ -26,7 +26,7 @@ else()
     CPMAddPackage(
         NAME capstone
         GITHUB_REPOSITORY capstone-engine/capstone
-        GIT_TAG 6.0.0-Alpha6
+        GIT_TAG 6.0.0-Alpha7
         OPTIONS
             "CAPSTONE_X86_ATT_DISABLE ON"
             "CAPSTONE_ALPHA_SUPPORT OFF"

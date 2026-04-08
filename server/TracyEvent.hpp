035a2ba30d65687b12f651f03d64146c28660019
Marcos Slomp <mslomp@gmail.com>
2026-04-08 07:16:09 -0700

    debug cleanup

--- server/TracyEvent.hpp
+++ server/TracyEvent.hpp
@@ -380,17 +380,16 @@ struct GpuEvent
     tracy_force_inline int64_t CpuEnd() const { return int64_t( _cpuEnd_thread ) >> 16; }
     tracy_force_inline void SetCpuEnd( int64_t cpuEnd ) { assert( cpuEnd < (int64_t)( 1ull << 47 ) ); memcpy( ((char*)&_cpuEnd_thread)+2, &cpuEnd, 4 ); memcpy( ((char*)&_cpuEnd_thread)+6, ((char*)&cpuEnd)+4, 2 ); }
     tracy_force_inline int64_t GpuStart() const { return int64_t( _gpuStart_child1 ) >> 16; }
-    tracy_force_inline void SetGpuStart( int64_t gpuStart ) {
-        // https://github.com/wolfpld/tracy/commit/c99dc5c43141e3359b10fb7e25c1e2e9ab91b82f
-        /*assert( gpuStart < (int64_t)( 1ull << 47 ) );*/
+    tracy_force_inline void SetGpuStart( int64_t gpuStart )
+    {
+        // Assert omitted for v0.6 trace compatibility
+        // assert( gpuStart < (int64_t)( 1ull << 47 ) );
         memcpy( ((char*)&_gpuStart_child1)+2, &gpuStart, 4 );
         memcpy( ((char*)&_gpuStart_child1)+6, ((char*)&gpuStart)+4, 2 );
     }
     tracy_force_inline int64_t GpuEnd() const { return int64_t( _gpuEnd_child2 ) >> 16; }
-    tracy_force_inline void SetGpuEnd( int64_t gpuEnd ) {
-        if (gpuEnd >= (int64_t)(1ull << 47)) {
-            fprintf(stderr, "ERROR: unexpected GpuEnd timestamp: %llu\n", gpuEnd);
-        }
+    tracy_force_inline void SetGpuEnd( int64_t gpuEnd )
+    {
         assert( gpuEnd < (int64_t)( 1ull << 47 ) );
         memcpy( ((char*)&_gpuEnd_child2)+2, &gpuEnd, 4 );
         memcpy( ((char*)&_gpuEnd_child2)+6, ((char*)&gpuEnd)+4, 2 );

aaf1304308d1494c96c31db17dde52bade3e6af2
Clément Grégoire <lectem@gmail.com>
2026-05-26 08:11:47 +0200

    Options.cmake: unify `set_options*` and `tracy_set_options`
    Renamed `tracy_*` versions to short version without prefix

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,54 +1,48 @@
-# Reusable option macros for CMake projects
+# Reusable option macros for Tracy CMake projects
 #
 # Usage:
-#   set_option(OPTION_NAME "Help text" ON/OFF)     - for boolean options
-#   set_option_value(VAR_NAME "Help text" "value") - for value options (CACHE STRING)
+#   set_option(OPTION_NAME "Help text" ON/OFF [TARGET])     - for boolean options
+#   set_option_value(VAR_NAME "Help text" "value" [TARGET]) - for value options (CACHE STRING)
+#   set_option_value_as_string(VAR_NAME "Help text" "value" [TARGET]) - for value options as C string literals
 #
-# The tracy_set_option* variants optionally propagate the option as a PUBLIC
-# compile definition on a target passed as trailing argument. Without a target
-# they behave identically to the bare helpers above.
+# [TARGET] is optional and specifies a target to which the option will 
+# be added as a compile definition (e.g., -DOPTION_NAME or -DVAR_NAME=value).
 
-# Boolean options (ON/OFF)
+# Boolean option (ON/OFF).
 macro(set_option option help value)
     option(${option} ${help} ${value})
     if(${option})
         message(STATUS "${option}: ON")
+        if(${ARGC} GREATER 3)
+            target_compile_definitions(${ARGV3} PUBLIC ${option})
+        endif()
     else()
         message(STATUS "${option}: OFF")
     endif()
 endmacro()
 
-# Value options (strings, numbers, etc.)
+# Value option (string/number).
 macro(set_option_value var help default)
     set(${var} ${default} CACHE STRING "${help}")
     if(${var})
         message(STATUS "${var}: ${${var}}")
+        if(${ARGC} GREATER 3)
+            target_compile_definitions(${ARGV3} PUBLIC ${var}=${${var}})
+        endif()
     else()
         message(STATUS "${var}: (not set)")
     endif()
 endmacro()
 
-# Boolean option, optionally propagated as PUBLIC compile definition.
-macro(tracy_set_option option help value)
-    set_option(${option} "${help}" ${value})
-    if(${option} AND ${ARGC} GREATER 3)
-        target_compile_definitions(${ARGV3} PUBLIC ${option})
-    endif()
-endmacro()
-
-# Value option, optionally propagated as PUBLIC compile definition (VAR=value).
-macro(tracy_set_option_value var help default)
-    set_option_value(${var} "${help}" "${default}")
-    if(${var} AND ${ARGC} GREATER 3)
-        target_compile_definitions(${ARGV3} PUBLIC ${var}=${${var}})
-    endif()
-endmacro()
-
-# Value option propagated as a C string literal (VAR="value").
-# For options whose value is a path or string consumed verbatim in C/C++ code.
-macro(tracy_set_option_value_as_string var help default)
-    set_option_value(${var} "${help}" "${default}")
-    if(${var} AND ${ARGC} GREATER 3)
-        target_compile_definitions(${ARGV3} PUBLIC "${var}=\"${${var}}\"")
+# Value option embedded as a C string literal (VAR="value").
+macro(set_option_value_as_string var help default)
+    set(${var} ${default} CACHE STRING "${help}")
+    if(${var})
+        message(STATUS "${var}: ${${var}}")
+        if(${ARGC} GREATER 3)
+            target_compile_definitions(${ARGV3} PUBLIC "${var}=\"${${var}}\"")
+        endif()
+    else()
+        message(STATUS "${var}: (not set)")
     endif()
 endmacro()

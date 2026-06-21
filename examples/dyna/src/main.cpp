8531e879a9737fad0526bef12bfbfc16b6ec0bb0
Bartosz Taudul <wolf@nereid.pl>
2026-06-21 22:40:41 +0200

    Use TracyNoop to ensure profiler library is linked with executable.

--- examples/dyna/src/main.cpp
+++ examples/dyna/src/main.cpp
@@ -2,9 +2,12 @@
 #include "gfx.hpp"
 
 #include <SDL3/SDL_main.h>
+#include <tracy/Tracy.hpp>
 
 int main( int /*argc*/, char* /*argv*/[] )
 {
+    TracyNoop;
+
     if( !dyna::Init::all() )
         return 1;
 

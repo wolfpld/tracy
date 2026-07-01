83298a3459c351dc9a87566c2232310c11faf022
Bartosz Taudul <wolf@nereid.pl>
2026-07-02 00:30:48 +0200

    Bump usearch.

--- cmake/vendor.cmake
+++ cmake/vendor.cmake
@@ -257,7 +257,7 @@ if(NOT EMSCRIPTEN)
     CPMAddPackage(
         NAME usearch
         GITHUB_REPOSITORY unum-cloud/usearch
-        GIT_TAG v2.25.2
+        GIT_TAG v2.25.3
         EXCLUDE_FROM_ALL TRUE
     )
 

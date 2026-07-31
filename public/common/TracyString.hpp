5ef415619db426b3094875c5ec73e82aa9c896f9
Bartosz Taudul <wolf@nereid.pl>
2026-07-31 23:02:36 +0200

    Add safe string copy.

---
+++ public/common/TracyString.hpp
@@ -0,0 +1,31 @@
+#ifndef __TRACYSTRING_HPP__
+#define __TRACYSTRING_HPP__
+
+#include <assert.h>
+#include <stddef.h>
+#include <string.h>
+
+namespace tracy
+{
+
+static inline size_t strzcpy( char* __restrict dst, const char* __restrict src, size_t dstSize )
+{
+    assert( dstSize > 0 );
+
+    const auto srcSz = strlen( src );
+    if( srcSz < dstSize )
+    {
+        memcpy( dst, src, srcSz + 1 );
+        return srcSz;
+    }
+    else
+    {
+        memcpy( dst, src, dstSize - 1 );
+        dst[dstSize - 1] = '\0';
+        return dstSize - 1;
+    }
+}
+
+}
+
+#endif

f032eba38934f34f595136ff0463f8f586906d84
Bartosz Taudul <wolf@nereid.pl>
2026-09-09 23:01:05 +0200

    Bump glfw to 3.5.1.

--- cmake/vendor.cmake
+++ cmake/vendor.cmake
@@ -130,7 +130,7 @@ if(VENDOR_GUI)
             CPMAddPackage(
                 NAME glfw
                 GITHUB_REPOSITORY glfw/glfw
-                GIT_TAG 3.4
+                GIT_TAG 3.5.1
                 OPTIONS
                     "GLFW_BUILD_EXAMPLES OFF"
                     "GLFW_BUILD_TESTS OFF"

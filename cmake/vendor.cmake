fb1b13a8a8aea30a45a798dd0dc90555323f7207
Bartosz Taudul <wolf@nereid.pl>
2026-09-10 20:43:02 +0200

    Bump nfd to 7bbbd9fe.

--- cmake/vendor.cmake
+++ cmake/vendor.cmake
@@ -231,7 +231,7 @@ if(VENDOR_GUI)
         CPMAddPackage(
             NAME nfd
             GITHUB_REPOSITORY btzy/nativefiledialog-extended
-            GIT_TAG 3cd252a8f7ca32419b1ca235c2990ba6a0ecba7c
+            GIT_TAG 7bbbd9fe6b1d1549b41df138f614d1a44df9ba08
             EXCLUDE_FROM_ALL TRUE
             PATCHES
                 "${CMAKE_CURRENT_LIST_DIR}/nfd-xdg-foreign-v2.patch"

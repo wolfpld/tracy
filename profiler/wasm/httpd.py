4332b43c3bf608c67ce36e22edfc3cbe3499fb12
Bartosz Taudul <wolf@nereid.pl>
2026-09-12 01:14:15 +0200

    Serve the web development files with CORS enabled.
    The server doubles as a host for trace files to be loaded by the web
    version from another origin, for example when profiling over the
    local network.

--- profiler/wasm/httpd.py
+++ profiler/wasm/httpd.py
@@ -9,6 +9,7 @@ class MyHTTPRequestHandler(server.SimpleHTTPRequestHandler):
         self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
         self.send_header("Pragma", "no-cache")
         self.send_header("Expires", "0")
+        self.send_header("Access-Control-Allow-Origin", "*");
         self.send_header("Cross-Origin-Embedder-Policy", "require-corp");
         self.send_header("Cross-Origin-Opener-Policy", "same-origin");
 
